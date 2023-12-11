2f401452235db24c462b249cdd3a90c33e4c1ab1
isbrakha <isaacbrakha14@gmail.com>
2023-12-11 14:57:26 -0500

    portfolio skeleton complete

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
